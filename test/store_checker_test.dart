6b163c941a67c501d204b95f59207c9e2945ee58
Francesco-FL <83022092+Francesco-FL@users.noreply.github.com>
2022-08-13 01:18:30 +0200

    fix test

--- test/store_checker_test.dart
+++ test/store_checker_test.dart
@@ -9,7 +9,7 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return Source.IS_INSTALLED_FROM_PLAY_STORE;
+      return Source.IS_INSTALLED_FROM_GOOGLE_PLAY_STORE;
     });
   });
 
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getSource', () async {
-    expect(await StoreChecker.getSource, Source.IS_INSTALLED_FROM_PLAY_STORE);
+    expect(await StoreChecker.getSource, Source.IS_INSTALLED_FROM_GOOGLE_PLAY_STORE);
   });
 }
